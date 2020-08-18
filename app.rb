c8188c6cc995550eb4cf87d496dd2e425327df2d
decent-scraper-1450 <jaimealba1574@gmail.com>
2020-08-18 16:27:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
+  
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do 
+    "My name is Jaime"
+  end
+
+  get '/hometown' do 
+    "My hometown is New York"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Shake It"
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,15 @@ Bundler.require(:default, ENV['SINATRA_ENV'])
 
 require './app'
 require_all 'models'
+  get '/name' do 
+    "My name is Victoria"
+  end
+
+  get '/hometown' do 
+    "My hometown is Charlottesville"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Shake It Off"
+  end
+end 
